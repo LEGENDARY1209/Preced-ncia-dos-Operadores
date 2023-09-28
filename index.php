640c21b5b1c0ef3d114964aac89bab46379f367c
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-09-28 15:42:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$a = 2;
+$b = 4;
+$c = 5;
+
+//Resultado igual 22
+$calculo = $a + $b * $c;
+ echo $calculo;
+ echo"<br>";
+
+//Resultado igual 10
+
+$calculo_2 = $b / $a *$c;
+echo $calculo_2;
+echo"<br>";
+
+$d = true;
+$e = false;
+$f = true;
+
+//Utilização de 2 operadores 1gicos 
+if ($d || $e && $f){
+
+//Condição
+
+}
+
+?>
